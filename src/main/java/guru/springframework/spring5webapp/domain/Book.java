c8e28b9f3d91c9c40ee643d31f2f277fdab64dd3
CodeClan <~}@~=N!1nizD&nxfg#7>
2021-10-04 10:27:31 +0200

    added thymeleaf template

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -42,7 +42,6 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", authors=" + authors +
                 '}';
     }
 
